e79462cd2b022882cec8acd578682415f5c7aa4a
cuki2 <mauricio@krebs.com.br>
2017-05-23 16:00:04 -0300

    Signed-off-by: cuki2 <mauricio@krebs.com.br>

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * painel.c
+ *
+ * Created: 23/05/2017 15:59:30
+ * Author : cuki
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
